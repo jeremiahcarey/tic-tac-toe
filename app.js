0a8499cdea3bcd2d20bbfcd28f2d44e858f9b89a
Jeremiah Carey <jeremiah.a.carey@gmail.com>
2022-06-28 14:43:47 -0400

    some logic began

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
-const gameSquares = document.querySelectorAll(".game-square");
 
-gameSquares.forEach((square) => {
-    square.addEventListener("click", function (e) {
-        e.target.children[0].innerText = "O";
-    });
-});
+
+const gameBoard = (() => {
+    const boardArray = [null, null, null,
+        null, "O", null,
+        null, null, null];
+
+    return { boardArray };
+})();
+
+const Player = (name, symbol) => {
+    let getName = () => name;
+    let getSymbol = () => symbol;
+    return { getName, getSymbol };
+}
+
+
+
+const gameControl = (() => {
+    const gameSquares = document.querySelectorAll(".game-square");
+    const updateGameDisplay = () => {
+        gameBoard.boardArray.forEach((square, index) => {
+            if (square) {
+                gameSquares[index].children[0].innerText = `${square}`;
+            }
+        })
+    };
+    const gamePlay = () => {
+        gameSquares.forEach((square) => {
+            square.addEventListener("click", function (e) {
+                e.target.children[0].innerText = `${currentPlayer.getSymbol()}`;
+                gameBoard.boardArray[e.target.dataset.index] = `${currentPlayer.getSymbol()}`;
+                updateGameDisplay();
+                if (currentPlayer = playerOne) {
+                    let currentPlayer = playerTwo;
+                } else {
+                    let currentPlayer = playerOne;
+                };
+            });
+        });
+    }
+
+    return { gameSquares, updateGameDisplay, gamePlay }
+})();
+
+const playerOne = Player("Player One", "X");
+const playerTwo = Player("Player Two", "O");
+let currentPlayer = playerTwo;
+gameControl.updateGameDisplay();
+gameControl.gamePlay();
+
+
+
